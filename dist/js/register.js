2f35f78adced84f8d6e7e96f67ab510cb862376c
Alejandro Pacheco Vargas <a.pacheco.v05@gmail.com>
2023-09-12 22:24:22 -0600

    Register modal created

--- dist/js/register.js
+++ dist/js/register.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const userButton = document.querySelector(".img-user button");
     const registrationPopup = document.getElementById("registration-popup");
-    const closePopupButton = document.querySelector(".close-popup-button"); // Selecciona la "X"
+    const closePopupButton = document.querySelector(".close-popup-button");
 
     userButton.addEventListener("click", function () {
         registrationPopup.style.display = "block";
